1a233c9059b80268ab17dcfd56bc1e1e5903d9ca
allancaixeta <allancaixeta@gmail.com>
2020-04-06 15:20:53 -0300

    Aula 1

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -13,6 +13,9 @@ public class Jogador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.Translate(1, 0, 0);
+        if (Input.GetKeyDown(KeyCode.A))
+            transform.Translate(-1, 0, 0);
     }
 }
