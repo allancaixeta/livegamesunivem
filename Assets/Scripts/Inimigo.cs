bc29456c20c8223bda86aba7a76700ca1dc0f55e
allancaixeta <allancaixeta@gmail.com>
2020-04-07 15:02:59 -0300

    Aula 2

--- Assets/Scripts/Inimigo.cs
+++ Assets/Scripts/Inimigo.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Inimigo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform player;
+    public float velocidade = 0.05f;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 direcaoInimigoParaJogador;
+        direcaoInimigoParaJogador = (player.position - transform.position);
+        direcaoInimigoParaJogador = direcaoInimigoParaJogador.normalized;
+        direcaoInimigoParaJogador = direcaoInimigoParaJogador * velocidade;
+        direcaoInimigoParaJogador.z = 0;
+        transform.Translate(direcaoInimigoParaJogador);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Jogador : MonoBehaviour
 {
+    public float velocidade = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +15,35 @@ public class Jogador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
-            transform.Translate(1, 0, 0);
-        if (Input.GetKeyDown(KeyCode.A))
-            transform.Translate(-1, 0, 0);
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(velocidade, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-velocidade, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, velocidade, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -velocidade, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //programe aqui oque fazer quando apertar espaço
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //programe aqui oque fazer quando apertar botao esquerdo
+
+        }
+
+
     }
 }
